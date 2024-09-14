31a9c230f2e84136c812763f0f9f1aeea45515ef
Keny-B <kenybeauzilebk@gmail.com>
2024-09-14 07:43:26 -0700

    at 7:42 AM upallnight

--- PayRollwiththebig3/src/main/java/com/gcu/allcontrollers/theController.java
+++ PayRollwiththebig3/src/main/java/com/gcu/allcontrollers/theController.java
@@ -3,7 +3,12 @@ package com.gcu.allcontrollers;
 // this class controll and answer the request of the web. hint: the returns (home,employee-signin, manager-signin, register)
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.gcu.model.Employee;
 
 @Controller
 @RequestMapping("/")
@@ -32,4 +37,26 @@ public class theController {
     public String register() {
         return "register"; 
     }
-}
+    @GetMapping("/manager/addnew-employee") // when manager signs in it can add new employee 
+    public String createEmployeeForm() {
+        return "addnew-employee"; 
+    }
+//  the manager deals with addin new employee
+    @PostMapping("/manager/addnew-employee")
+    public ModelAndView createEmployee(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password) {
+
+        // Manager add a new employee
+        Employee employee = new Employee();
+        employee.setusername(username);
+        employee.setpassword(password);
+
+        // extra things can be added for exampl saving the employee to a database
+        System.out.println("Manager created Employee: " + username);
+        //after done send the manager back to his dashboard or to the beginnig page
+        return new ModelAndView("redirect:/");
+    }
+        
+ }
+
