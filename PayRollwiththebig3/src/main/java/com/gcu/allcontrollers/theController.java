09b6b97d79433910e6c3e34900fa3b43a80f07ca
Keny-B <kenybeauzilebk@gmail.com>
2024-09-27 18:07:27 -0700

    .

--- PayRollwiththebig3/src/main/java/com/gcu/allcontrollers/theController.java
+++ PayRollwiththebig3/src/main/java/com/gcu/allcontrollers/theController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.boot.autoconfigure.security; 
+import org.springframework.web.servlet.ModelAndView; 
+
 import com.gcu.model.Employee;
 import com.gcu.model.User;
 
