88018f3d1454a07fe7b03fbb0ca23f3edce375e8
StarGazer53 <94339193+StarGazer53@users.noreply.github.com>
2024-09-27 18:12:40 -0700

    some edits

--- PayRollwiththebig3/src/main/java/com/gcu/PayRollwiththebig3Application.java
+++ PayRollwiththebig3/src/main/java/com/gcu/PayRollwiththebig3Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class PayRollwiththebig3Application {
+public class PayRollwiththebig3Application 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(PayRollwiththebig3Application.class, args);
 	}
 
